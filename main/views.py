dbc438e69a23221d102acf567ed1f6b14e7a9a02
ariana <ariana.nurlayla@ui.ac.id>
2023-09-20 04:49:16 +0700

    add missing functions

--- main/views.py
+++ main/views.py
@@ -27,3 +27,24 @@ def create_product(request):
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+def show_html(request):
+    data = Item.objects.all()
+    context = {'items': data}
+    return render(request, "show_table.html", context)
+
+def show_xml(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
