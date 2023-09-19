a6c0d5bf34e0564ba41e0d2e0a73f4bb4cf245bf
ariana <ariana.nurlayla@ui.ac.id>
2023-09-20 04:42:01 +0700

    membuat input form dan menampilkan objek

--- main/views.py
+++ main/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.http import HttpResponseRedirect
+from main.forms import ProductForm
+from django.urls import reverse
+from main.models import Item
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
+    items = Item.objects.all()
+    jumlah_item = len(items)
     context = {
         'name': 'Ariana Nurlayla Syabandini',
         'class': 'PBP C',
-        'stok': [
-            {
-                'name': 'Cokelat',
-                'amount': 2,
-                'description': 'jajan',
-            },
-            {
-                'name': 'Biskuit',
-                'amount': 5,
-                'description': 'jajan',
-            }
-        ]
+        'items': items,
+        'jumlah_item': jumlah_item,
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
